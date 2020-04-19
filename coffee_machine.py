798e9ec8bbc8e04065d4d9733d3c41977191883a
Youssef alaa <yalaa129@gmail.com>
2020-04-19 20:29:47 +0300

    asdf

---
+++ coffee_machine.py
@@ -0,0 +1,68 @@
+water = 400
+milk = 540
+coffee_beans = 120
+disp_cups = 9
+money = 550
+
+
+def buy():
+    def coffee_maker(water_, milk_, coffee_, money_):
+        if water_ <= water and milk_ <= milk and coffee_beans <= coffee_beans:
+            print('I have enough resources, making you a coffee!\n')
+            globals()['water'] -= water_
+            globals()['milk'] -= milk_
+            globals()['coffee_beans'] -= coffee_
+            globals()['disp_cups'] -= 1
+            globals()['money'] += money_
+        else:
+            if water_ > water:
+                print('Sorry, not enough water!\n')
+            elif milk_ > milk:
+                print('Sorry, not enough milk!\n')
+            elif coffee_ > coffee_beans:
+                print('Sorry, not enough coffee beans!\n')
+
+    order = input('\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:\n')
+
+    if order == '1':
+        coffee_maker(250, 0, 16, 4)
+    elif order == '2':
+        coffee_maker(350, 75, 20, 7)
+    elif order == '3':
+        coffee_maker(200, 100, 12, 6)
+    elif order == 'back':
+        pass
+
+
+def fill():
+    globals()['water'] += int(input('\nWrite how many ml of water do you want to add:\n'))
+    globals()['milk'] += int(input('Write how many ml of milk do you want to add:\n'))
+    globals()['coffee_beans'] += int(input('Write how many grams of coffee beans do you want to add:\n'))
+    globals()['disp_cups'] += int(input('Write how many disposable cups do you want to add:\n'))
+    print('')
+
+
+def take():
+    global money
+
+    print(f'\nI gave you ${money}\n')
+    money = 0
+
+
+while True:
+    action = input('Write action (buy, fill, take, remaining, exit):\n')
+    if action == 'buy':
+        buy()
+    elif action == 'fill':
+        fill()
+    elif action == 'take':
+        take()
+    elif action == 'remaining':
+        print(f'''\nThis coffee machine has:
+{water} of water
+{milk} of milk
+{coffee_beans} of coffee beans
+{disp_cups} of disposable cups''')
+        print(f'${money} of money' if money > 0 else '0 of money')
+    elif action == 'exit':
+        break
